7f4b7e12ff9ab4a52acfe2e965892bd46d80b224
Kartik <kbhasin2003@gmail.com>
2023-07-07 03:06:52 +0530

    late night commit

--- Arrays/merge2Sorted_Arrays.cpp
+++ Arrays/merge2Sorted_Arrays.cpp
@@ -1,40 +1,35 @@
 #include<iostream>
+#include<vector>
 using namespace std;
-int main(){
-int n=7;
-
-int arr1[7]={3,4,61,82,84,85,87};
-int arr2[8]={1,5,7,10,55,98,99,100};
-int m=8;
-int arr[15]={};
-int k=15;
+// MERGE TWO SORTED ARRAYS
+void merge(vector<int>&v,vector<int>& nums1, vector<int>& nums2){
+     int m=nums1.size();
+     int n=nums2.size();
 int i=0;
 int j=0;
-int x=0;
-while((i<=n &&j<m) ||(j<=m &&i<n) ){
-     if(j==m && i<n){
-arr[x++]=arr1[i++];
-continue;
-    }
-     if(i==n&& j<m){
-arr[x++]=arr2[j++];
-continue;
-    }
-
-    if(arr1[i]>=arr2[j]){
-        arr[x++]=arr2[j++];
-
-    }
-    
-    else if(arr1[i]<=arr2[j]){
-        arr[x++]=arr1[i++];
-    
-    }
-   
-    if(x==k)break;
+int index=0;
+while(i<m && j<n){
+    if(nums1[i]<=nums2[j])v[index++]=nums1[i++];
+    else if(nums1[i]>nums2[j])v[index++]=nums2[j++];
 }
-for(int y=0;y<k;y++){
-    cout<<arr[y]<<" ";
-}
+while(i<m)v[index++]=nums1[i++];
+while(j<n)v[index++]=nums2[j++];
+ }
+ // Leetcode
+    double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
+         int m=nums1.size();
+     int n=nums2.size();
+        vector<int>ans(m+n,-1);
+        merge(ans,nums1,nums2);
+double median=-1;
+        if((m+n)&1)median=ans[(m+n)/2];
+        else median=(ans[(m+n)/2]+ans[(m+n)/2-1])/2.0;
+        return median;
+    }
+int main(){
+int n=15;
+int arr1[7]={3,4,61,82,84,85,87};
+int arr2[8]={1,5,7,10,55,98,99,100};
+
 return 0;
 }
